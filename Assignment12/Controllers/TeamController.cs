dbcee84ccad5d132629af22a8403f7ef74103d29
onkar <jadhavonkar333@gmail.com>
2023-12-26 16:06:28 +0530

    assignment 12

---
+++ Assignment12/Controllers/TeamController.cs
@@ -0,0 +1,25 @@
+﻿using Assignment12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment12.Controllers
+{
+    public class TeamController : Controller
+    {
+        static List<Team> teamList = new List<Team>()
+{
+    new Team(){Id=1,Name="India",NOWC="2"},
+    new Team(){Id=2,Name="Australia",NOWC="4"},
+    new Team(){Id=3,Name="Westindies",NOWC="2"},
+    new Team(){Id=4,Name="England",NOWC="1"}
+};
+        // GET: Team
+        public ActionResult Index()
+        {
+            return View(teamList);
+        }
+    }
+}
---
+++ Assignment12/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment12.Models
+{
+    
+        public class Team
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string NOWC { get; set; }
+        }
+    
+}
